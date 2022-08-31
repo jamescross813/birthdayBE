44606fecacf3b19fc79d269dbcf8e36f18160a9e
jamescross813 <crossjames813@gmail.com>
2022-08-31 01:55:03 -0500

    initial set up

---
+++ src/test/java/com/birthdaybe/birthdaybe/BirthdayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birthdaybe.birthdaybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
