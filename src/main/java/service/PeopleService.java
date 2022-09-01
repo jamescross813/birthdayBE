6e91885265180b5f096f562575d8de7d571c4b29
jamescross813 <crossjames813@gmail.com>
2022-09-01 17:27:28 -0500

    autowired repo

--- src/main/java/service/PeopleService.java
+++ src/main/java/service/PeopleService.java
@@ -2,11 +2,15 @@ package service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.PeopleRepository;
 
 @Service
 public class PeopleService {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    PeopleRepository peopleRepo;
 }
