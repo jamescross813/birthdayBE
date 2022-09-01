df25eb04d3f6aa6ce2ed2e55ef3d343260e6dc32
jamescross813 <crossjames813@gmail.com>
2022-09-01 17:25:01 -0500

    logger set up

--- src/main/java/service/PeopleService.java
+++ src/main/java/service/PeopleService.java
@@ -1,8 +1,12 @@
 package service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PeopleService {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
 }
