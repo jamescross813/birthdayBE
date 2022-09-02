230bf2e941c5f315e04e6bcce8c5de174d7103f6
jamescross813 <crossjames813@gmail.com>
2022-09-02 17:25:36 -0500

    started getListOfBirthdays method

--- src/main/java/service/PeopleService.java
+++ src/main/java/service/PeopleService.java
@@ -1,12 +1,14 @@
 package service;
 
 import dto.PeopleDTO;
+import entity.People;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.PeopleRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +20,8 @@ public class PeopleService {
     PeopleRepository peopleRepo;
 
     public List<PeopleDTO> getListOfBirthdays(String date){
+        List<PeopleDTO> peopDTO = new ArrayList();
+
         return null;
     }
 
