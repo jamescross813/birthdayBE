82bfe552aa9e6fecb3906cf61dc1f2fb1bd53220
jamescross813 <crossjames813@gmail.com>
2022-09-06 16:29:21 -0500

    logger added

--- src/main/java/controller/PeopleController.java
+++ src/main/java/controller/PeopleController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class PeopleController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
 }
