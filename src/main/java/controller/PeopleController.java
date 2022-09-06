c18a5613624e228cea952b3256e3fca2862aed67
jamescross813 <crossjames813@gmail.com>
2022-09-06 16:30:16 -0500

    autowired to service

--- src/main/java/controller/PeopleController.java
+++ src/main/java/controller/PeopleController.java
@@ -2,8 +2,10 @@ package controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import service.PeopleService;
 
 @RestController
 @CrossOrigin
@@ -11,4 +13,6 @@ public class PeopleController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    PeopleService peopService;
 }
